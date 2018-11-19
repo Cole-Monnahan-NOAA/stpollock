d832077cc989cf47eed6aed37dd24ee651e32414
colemonnahan <monnahc@uw.edu>
2018-11-19 11:57:03 -0800

    Add outline of simulation code

---
+++ simulator.R
@@ -0,0 +1,130 @@
+#' Generate spatio-temporal index
+#'
+#' @return A data.frame containing the spatial locations and densities
+#'   associated for each year
+generate.density <- function(st.list, nyrs, X){
+  NULL
+}
+
+#' Distribute the spatial density in the vertical dimension
+#' @param density The output data.frame from generate.density function.
+#' @return The density cbinded with the binned densities
+distribution.density <- function(density, vertical.trend, X, vbins, obins){
+  NULL
+}
+
+#' Sample from the 3D density surface for the two gear types.
+#'
+#' @param density The density output from distribute.density.
+#' @return A data frame of locations and sampled gear types
+sample.data <- function(density, bt.cv, at.cv, pl.list, obins){
+  NULL
+}
+
+#' Prepare simulated inputs for the different model types
+#'
+#' @param data The output from sample.data
+#' @return A list of lists for the data inputs for all of the model types
+prepare.inputs <- function(data){
+
+  ## Setup the VAST inputs
+  Method <- c("Grid", "Mesh", "Spherical_mesh")[2]
+  grid_size_km <- 50
+  n_x <- 100
+  ## Model settings
+  FieldConfig <- c(Omega1=3, Epsilon1=0, Omega2=0, Epsilon2=0)
+  RhoConfig <- c(Beta1=0, Beta2=0, Epsilon1=0, Epsilon2=0)
+  OverdispersionConfig <- c(Delta1=0, Delta2=0)
+  ObsModel <- c(1,1)
+  Options <-  c(SD_site_density=0, SD_site_logdensity=0, Calculate_Range=1,
+                Calculate_evenness=0, Calculate_effective_area=1, Calculate_Cov_SE=0,
+                Calculate_Synchrony=0, Calculate_Coherence=0)
+  ## Stratification for results
+  strata.limits <- data.frame(STRATA=All_areas)
+  ## Derived objects
+  Region <- "Eastern_Bering_Sea"
+  ## Save settings
+  DateFile <- paste0(getwd(),'/VAST_output/')
+  dir.create(DateFile)
+  Record <- list("Version"="VAST_v4_0_0","Method"=Method,"grid_size_km"=grid_size_km,"n_x"=n_x,"FieldConfig"=FieldConfig,"RhoConfig"=RhoConfig,"OverdispersionConfig"=OverdispersionConfig,"ObsModel"=ObsModel,"Region"=Region,"strata.limits"=strata.limits)
+  save( Record, file=file.path(DateFile,"Record.RData"))
+  capture.output( Record, file=paste0(DateFile,"Record.txt"))
+  TmbDir <- DateFile
+  TmbData <- Data_Fn(Version="VAST_v4_0_0", FieldConfig=FieldConfig,
+                      OverdispersionConfig=OverdispersionConfig,
+                      RhoConfig=RhoConfig, ObsModel=ObsModel, c_iz=c_iz,
+                      b_i=b_i, a_i=Data_Geostat[,'AreaSwept_km2'],
+                      v_i=rep(factor(1:110-1), times=3),
+                      s_i=Data_Geostat[,'knot_i']-1,
+                      t_i=Data_Geostat[,'Year'], a_xl=Spatial_List$a_xl,
+                      MeshList=Spatial_List$MeshList,
+                      GridList=Spatial_List$GridList,
+                      Method=Spatial_List$Method, Options=Options,
+                      Aniso=FALSE)
+
+  ## Now two independent ST indices with VAST
+
+
+  ## And the combined model from Stan's paper
+
+
+  NULL
+}
+
+
+#' Run a single replicate of the simulation
+#' @param replicate The replicate number for bookkeeping and setting seed.
+#' @param st.list A list of geospatial parameters
+#' @param nyrs The number of years to run
+#' @param abundance.trend The trend in abundance per year
+#' @param vertical.trend A trend in the mean vertical distribution
+#' @param vbins The number of vertical bins used to approximate the
+#'   vertical density. These are scaled automatically to the water column
+#'   height (thus bin widths vary with depth).
+#' @param X A matrix of environmental covariates associated for each
+#'   spatial point
+#' @param bt.cv The variance for the BT sampling process.
+#' @param at.cv The variance for the AT sampling process.
+#' @param pl.list Other Poisson-link parameters?
+#' @param obins The observation bins, assuming 0-3, 3-16, 16-surface
+simulate <- function(replicate, st.list, nyrs, abundance.trend,
+                     vertical.trend, vbins, X, bt.cv, at.cv, pl.list,
+                     obins){
+  ## Check inputs
+
+  ## Generate 2D density
+  set.seed(replicate)
+  den2d <- generate.density(st.list, nyrs, X.space, beta.space)
+
+  ## Distribution density vertically
+  den3d <- distribute.density(den2d, vertical.trend, X.vert, beta.vert,
+                              vbins, obins)
+
+  ## Simulate the sampling process for both gear types
+  data <- sample.data(den3d, bt.cv, at.cv, pl.list, obins)
+
+  ## Reorganize data for models
+  inputs <- prepare.inputs(data)
+
+  ## Fit the full VAST model
+  Version <- ''
+  x <- inputs$vast.full
+  build.full <- Build_TMB_Fn(TmbData=x$TmbData, RunDir=DateFile,
+                          Version=Version,  RhoConfig=x$RhoConfig,
+                          loc_x=x$Spatial_List$loc_x, Method=x$Method,
+                          TmbDir=TmbDir, Random=Random)
+  obj.full <- build.full$obj; obj.full$env$beSilent()
+  ## Not sure why passing lower and upper throws an error for this case
+  Opt.full <- Optimize( obj=obj3, savedir=DateFile, getsd=TRUE,
+                   lower=build.full$lower, upper=build.full$upper,
+                   control=list(trace=0))
+  rep.full <- obj.full$report()
+
+  ## Fit the independent VAST models, one for BT and one for AT
+
+  ## and the combined model
+
+  NULL
+
+}
+
