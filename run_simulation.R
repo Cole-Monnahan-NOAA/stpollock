a17005d975e737efae194ce20bd71278eb8416d0
colemonnahan <monnahc@uw.edu>
2018-12-04 16:33:11 -0800

    Add more realism to simulation

--- run_simulation.R
+++ run_simulation.R
@@ -10,7 +10,7 @@ source("simulator.R")
 ## Basic simulation
 set.seed(1)
 atrend <- sort(rnorm(10))
-st.list <- list(lon=runif(100), lat=runif(100),
+st.list <- list(lon=runif(100), lat=runif(100), beta0=3,
                 depth = sample(50:100, size=100, replace=TRUE))
 
 out <- simulate(replicate=1, st.list=st.list, nyrs=10, abundance.trend=atrend)
