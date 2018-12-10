47f9645830e2187873a256aa49fa2f2011d5e797
colemonnahan <monnahc@uw.edu>
2018-12-10 15:52:07 -0800

    Add first pass of scripts to fit to the real data

---
+++ fit_real_data.R
@@ -0,0 +1,92 @@
+## Central file to fit the pollock data using the modified VAST model
+
+## library(devtools)
+## remove.packages('VAST')
+## install_github('James-Thorson/VAST')
+library(VAST)
+library(TMB)
+library(maps)
+library(mapdata)
+source("load_read_data.R")
+
+
+
+TmbList = Build_TMB_Fn("TmbData"=TmbData, "RunDir"=DateFile,
+                  "Version"=Version, "RhoConfig"=RhoConfig,
+                  "loc_x"=Spatial_List$loc_x, "Method"=Method)
+Obj = TmbList[["Obj"]]
+Obj$env$beSilent()
+Opt = TMBhelper::Optimize( obj=Obj, lower=TmbList[["Lower"]],
+                  upper=TmbList[["Upper"]], getsd=TRUE, savedir=DateFile,
+                  bias.correct=FALSE, newtonsteps=1, control=list(trace=10))
+
+Report = Obj$report()
+Save = list("Opt"=Opt, "Report"=Report, "ParHat"=Obj$env$parList(Opt$par), "TmbData"=TmbData)
+save(Save, file=paste0(DateFile,"Save.RData"))
+
+## Manually calculate SE for the total biomass index. Since it's a sum of
+## the three the derivatives are all 1 and so the SE is the sqrt of the sum
+## of all of the variances and covariances. This feature is not coded
+## into VAST yet so have to do it manually. also note that the order of
+## the Index_cyl matrix in vector form is Index_11, Index_21, Index_31,
+## Index_12,.. etc. This effects the subsetting below
+
+est <- data.frame(par=names(Opt$SD$value), value=Opt$SD$value,
+                  se=Opt$SD$sd)
+tmp <- which(names(Opt$SD$value) %in% 'Index_cyl')
+cov.index <- Opt$SD$cov[tmp,tmp]
+index <- data.frame(year=1,
+                    value=apply(Report$Index_cyl, 2, sum),
+                    se=sqrt(sapply(1, function(i) {j=1:3+3*(i-1); sum(cov.index[j,j])})))
+
+
+
+plot_data(Extrapolation_List=Extrapolation_List, Spatial_List=Spatial_List, Data_Geostat=Data_Geostat, PlotDir=DateFile )
+pander::pandoc.table( Opt$diagnostics[,c('Param','Lower','MLE','Upper','final_gradient')] )
+Enc_prob = plot_encounter_diagnostic( Report=Report, Data_Geostat=Data_Geostat, DirName=DateFile)
+Q = plot_quantile_diagnostic( TmbData=TmbData, Report=Report, FileName_PP="Posterior_Predictive",
+                             FileName_Phist="Posterior_Predictive-Histogram",
+                             FileName_QQ="Q-Q_plot", FileName_Qhist="Q-Q_hist", DateFile=DateFile )
+
+MapDetails_List = make_map_info( "Region"=Region, "NN_Extrap"=Spatial_List$PolygonList$NN_Extrap, "Extrapolation_List"=Extrapolation_List )
+                                        # Decide which years to plot
+Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
+Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
+plot_residuals(Lat_i=Data_Geostat[,'Lat'], Lon_i=Data_Geostat[,'Lon'],
+               TmbData=TmbData, Report=Report, Q=Q, savedir=DateFile,
+               MappingDetails=MapDetails_List[["MappingDetails"]],
+               PlotDF=MapDetails_List[["PlotDF"]],
+               MapSizeRatio=MapDetails_List[["MapSizeRatio"]],
+               Xlim=MapDetails_List[["Xlim"]],
+               Ylim=MapDetails_List[["Ylim"]], FileName=DateFile,
+               Year_Set=Year_Set, Years2Include=Years2Include,
+               Rotate=MapDetails_List[["Rotate"]],
+               Cex=MapDetails_List[["Cex"]],
+               Legend=MapDetails_List[["Legend"]],
+               zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0),
+               oma=c(3.5,3.5,0,0), cex=1.8)
+
+## plot_anisotropy( FileName=paste0(DateFile,"Aniso.png"), Report=Report,
+##                 TmbData=TmbData )
+## Dens_xt = plot_maps(plot_set=c(3),
+##                 MappingDetails=MapDetails_List[["MappingDetails"]],
+##                 Report=Report, Sdreport=Opt$SD,
+##                 PlotDF=MapDetails_List[["PlotDF"]],
+##                 MapSizeRatio=MapDetails_List[["MapSizeRatio"]],
+##                 Xlim=MapDetails_List[["Xlim"]],
+##                 Ylim=MapDetails_List[["Ylim"]], FileName=DateFile,
+##                 Year_Set=Year_Set, Years2Include=Years2Include,
+##                 Rotate=MapDetails_List[["Rotate"]],
+##                 Cex=MapDetails_List[["Cex"]],
+##                 Legend=MapDetails_List[["Legend"]],
+##                 zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0),
+##                 oma=c(3.5,3.5,0,0), cex=1.8, plot_legend_fig=FALSE)
+## Dens_DF = cbind( "Density"=as.vector(Dens_xt),
+##                 "Year"=Year_Set[col(Dens_xt)],
+##                 "E_km"=Spatial_List$MeshList$loc_x[row(Dens_xt),'E_km'],
+##                 "N_km"=Spatial_List$MeshList$loc_x[row(Dens_xt),'N_km'] )
+
+Index = plot_biomass_index( DirName=DateFile, TmbData=TmbData, Sdreport=Opt[["SD"]], Year_Set=Year_Set, Years2Include=Years2Include, use_biascorr=TRUE )
+pander::pandoc.table( Index$Table[,c("Year","Fleet","Estimate_metric_tons","SD_log","SD_mt")] )
+
+plot_range_index(Report=Report, TmbData=TmbData, Sdreport=Opt[["SD"]], Znames=colnames(TmbData$Z_xm), PlotDir=DateFile, Year_Set=Year_Set)
---
+++ load_read_data.R
@@ -0,0 +1,65 @@
+Version <- "VAST_v5_3_0"
+
+bts <- read.csv('data/bts.csv')
+ats <- read.csv('data/ats.csv')
+
+bts <- subset(bts, year==2007)
+ats <- subset(ats, year==2007)
+
+Method = c("Grid", "Mesh", "Spherical_mesh")[2]
+grid_size_km = 50
+n_x = 100
+## Model settings
+FieldConfig = c("Omega1"=3, "Epsilon1"=0, "Omega2"=0, "Epsilon2"=0)
+RhoConfig = c("Beta1"=0, "Beta2"=0, "Epsilon1"=0, "Epsilon2"=0)
+OverdispersionConfig = c("Delta1"=0, "Delta2"=0)
+ObsModel = c(1,1)
+Options =  c("SD_site_density"=0, "SD_site_logdensity"=0, "Calculate_Range"=1, "Calculate_evenness"=0, "Calculate_effective_area"=1, "Calculate_Cov_SE"=0, 'Calculate_Synchrony'=0, 'Calculate_Coherence'=0)
+## Stratification for results
+strata.limits <- data.frame('STRATA'="All_areas")
+## Derived objects
+Region = "Eastern_Bering_Sea"
+## Save settings
+DateFile = paste0(getwd(),'/VAST_output_real/')
+dir.create(DateFile)
+Record = list("Version"=Version,"Method"=Method,"grid_size_km"=grid_size_km,"n_x"=n_x,"FieldConfig"=FieldConfig,"RhoConfig"=RhoConfig,"OverdispersionConfig"=OverdispersionConfig,"ObsModel"=ObsModel,"Region"=Region,"strata.limits"=strata.limits)
+save( Record, file=file.path(DateFile,"Record.RData"))
+capture.output( Record, file=paste0(DateFile,"Record.txt"))
+TmbDir <- DateFile
+DF_p1 = data.frame( Lat=bts$lat, Lon=bts$lon, Year=bts$year,
+                   Catch_KG=bts$density, Gear='Trawl', AreaSwept_km2=1,
+                   Vessel='none')
+DF_p2 = data.frame( Lat=ats$lat, Lon=ats$lon, Year=ats$year,
+                   Catch_KG=ats$strata2, Gear='Acoustic_3-16', AreaSwept_km2=1,
+                   Vessel='none')
+DF_p3 = data.frame( Lat=ats$lat, Lon=ats$lon, Year=ats$year,
+                   Catch_KG=ats$strata3, Gear='Acoustic_16-surface', AreaSwept_km2=1,
+                   Vessel='none')
+Data_Geostat = rbind( DF_p1, DF_p2, DF_p3 )
+Extrapolation_List =
+  make_extrapolation_info( Region=Region, strata.limits=strata.limits )
+## Derived objects for spatio-temporal estimation
+Spatial_List = make_spatial_info( grid_size_km=grid_size_km, n_x=n_x,
+                                 Method=Method, Lon=Data_Geostat[,'Lon'],
+                                 Lat=Data_Geostat[,'Lat'],
+                                 Extrapolation_List=Extrapolation_List,
+                                 DirPath=DateFile, Save_Results=FALSE )
+Data_Geostat = cbind( Data_Geostat, "knot_i"=Spatial_List$knot_i )
+c_iz = matrix( c(1,2, 2,NA, 3,NA), byrow=TRUE, nrow=3,
+              ncol=2)[as.numeric(Data_Geostat[,'Gear']),] - 1
+# Add threshold
+b_i = Data_Geostat[,'Catch_KG']
+# Build data
+TmbData = Data_Fn(Version=Version, FieldConfig=FieldConfig,
+                  OverdispersionConfig=OverdispersionConfig,
+                  RhoConfig=RhoConfig, ObsModel=ObsModel, c_iz=c_iz,
+                  b_i=b_i, a_i=Data_Geostat[,'AreaSwept_km2'],
+                  v_i=as.numeric(Data_Geostat[,'Vessel'])-1,
+                  s_i=Data_Geostat[,'knot_i']-1,
+                  t_i=Data_Geostat[,'Year'], a_xl=Spatial_List$a_xl,
+                  MeshList=Spatial_List$MeshList,
+                  GridList=Spatial_List$GridList,
+                  Method=Spatial_List$Method, Options=Options,
+                  Aniso=FALSE)
+Random = "generate"
+
