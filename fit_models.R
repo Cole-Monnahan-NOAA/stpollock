70f6dc69f13ccbf0d7ebb14af8c5de824abd2466
colemonnahan <monnahc@uw.edu>
2018-12-14 15:43:35 -0800

    Add some quick code to plot results across model versions

--- fit_models.R
+++ fit_models.R
@@ -4,8 +4,10 @@ source('startup.R')
 
 
 n_x <- 500 # number of knots
-model <- c('ats', 'bts', 'combined')[3]
-space <- c('NS', 'S', 'ST')[3]
+for(m in 1:3){
+for(s in 2:3){
+model <- c('ats', 'bts', 'combined')[m]
+space <- c('NS', 'S', 'ST')[s]
 source("prepare_inputs.R")
 Opt <- Optimize(obj=Obj, lower=TmbList$Lower,
                 upper=TmbList$Upper,  savedir=savedir,
@@ -13,3 +15,49 @@ Opt <- Optimize(obj=Obj, lower=TmbList$Lower,
 ## TMBhelper::Check_Identifiable(Obj)
 results <- process.results(Opt, Obj, model, space, savedir)
 plot.vastfit(results)
+}
+}
+
+
+## Process them
+xx <- list.dirs(, full.names=FALSE, recursive=FALSE)
+dirs <- xx[grep('VAST_', xx)]
+
+indices <- ldply(dirs, function(x) {
+  ff <- file.path(x, 'Save.RData')
+  if(file.exists(ff)){
+    load(ff)
+    return(Save$Index)
+  } else {
+    return(NULL)
+  }
+})
+ggplot(indices, aes(year, est, group=strata, color=strata,  fill=strata)) +
+  geom_ribbon(aes(ymin=lwr, ymax=upr), alpha=.33) +
+  geom_line() + geom_point()+
+  facet_grid(model~space)
+
+ests <- ldply(dirs, function(x) {
+  ff <- file.path(x, 'Save.RData')
+  if(file.exists(ff)){
+    load(ff)
+    m <- Save$Index$model[1]; s <- Save$Index$space[1]
+    est <- data.frame(model=m, space=s, par=names(Save$Opt$SD$par.fixed),
+                      est=Save$Opt$SD$par.fixed,
+                      se=sqrt(diag(Save$Opt$SD$cov.fixed)), stringsAsFactors=FALSE)
+    ## Add a number after each par to make them unique for plotting
+    tmp <- as.vector(do.call(c, sapply(unique(est$par), function(x) {
+      y <- est$par[est$par==x]
+      1:length(y)})))
+    est$par2 <- paste(est$par, tmp, sep='_')
+    est$parnum <- tmp
+    return(est)
+  } else {
+    return(NULL)
+  }
+})
+ests$parnum <- ests$parnum+rnorm(nrow(ests), 0,.1)
+
+ggplot(ests, aes(parnum, est, color=model, shape=space)) +
+  geom_point(size=3) + facet_wrap('par', scales='free') +
+  geom_linerange(aes(ymin=est-2*se, ymax=est+2*se), lwd=1.5)
