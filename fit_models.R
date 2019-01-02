cf76a6cb4e5c6062bc915dd6a90b9d121c94cb30
colemonnahan <monnahc@uw.edu>
2019-01-02 08:03:29 -0800

    Add a quick MCMC test

--- fit_models.R
+++ fit_models.R
@@ -5,14 +5,14 @@ source('startup.R')
 
 n_x <- 300 # number of knots
 for(m in 1:3){
-for(s in 2){
+for(s in 1:2){
 model <- c('ats', 'bts', 'combined')[m]
 space <- c('NS', 'S', 'ST')[s]
 source("prepare_inputs.R")
 Opt <- Optimize(obj=Obj, lower=TmbList$Lower,
                 upper=TmbList$Upper,  savedir=savedir,
                 newtonsteps=1, control=list(trace=10))
-## TMBhelper::Check_Identifiable(Obj)
+ TMBhelper::Check_Identifiable(Obj)
 results <- process.results(Opt, Obj, model, space, savedir)
 plot.vastfit(results)
 }
@@ -21,9 +21,15 @@ plot.vastfit(results)
 
 ### Some MCMC tests
 library(tmbstan)
-options(mc.cores = 4)
-n_x <- 400
+library(shinystan)
+options(mc.cores = 7)
+n_x <- 200
 model <- 'combined'
 space <- 'S'
 source("prepare_inputs.R")
-mcmc <- tmbstan(obj=Obj, iter=1000, chains=4, lower=TmbList$Lower, upper=TmbList$Upper)
+mcmc <- tmbstan(obj=Obj, iter=1000, chains=7, lower=TmbList$Lower,
+                init='last.par.best',
+                upper=TmbList$Upper, control=list(max_treedepth=12),
+                open_progress=FALSE)
+saveRDS(mcmc, file='mcmc.RDS')
+launch_shinystan(mcmc)
