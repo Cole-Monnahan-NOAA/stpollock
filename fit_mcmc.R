adf9ae1a949d0732de031a60474b049f2eb29173
colemonnahan <monnahc@uw.edu>
2019-04-24 12:25:14 -0700

    Add quick test of L_omega1 which is funky

--- fit_mcmc.R
+++ fit_mcmc.R
@@ -52,3 +52,48 @@ fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
 saveRDS(object = fit, file=paste0(savedir,'/mcmcfit.RDS'))
 plot.mcmc(Obj, savedir, fit)
 
+
+
+
+
+
+
+### ## Quick exploration of L_omega1
+df <- as.data.frame(fit)
+xx <- df[,grep('L_omega1', x=names(df))]
+## Each row is a covariance matrix.. see if these are different when L4 is
+## positive vs negative
+pos <- xx[,4]>0
+hist(xx[,4])
+
+L_to_cov <- function(Lvec, ncol=2, nrow=3){
+  Lvec <- as.numeric(Lvec)
+  L <- matrix(0, nrow, ncol)
+  counter <- 1
+  for(r in 1:nrow){
+    for(c in 1:ncol){
+      if(r>=c){
+        L[r,c] <- Lvec[counter]
+        counter <- counter+1
+      }
+    }
+  }
+  covar <- L %*% t(L)
+  ## Return vector of just the 6 lower triangular elements
+  covar[lower.tri(covar, diag=TRUE)]
+}
+
+covs <- data.frame(t(apply(xx, 1, L_to_cov)))
+names(covs) <- paste0('cov_vec', 1:6)
+png('plots/cov_vec_test.png', width=9, height=7, res=500, units='in')
+pairs(covs, col=ifelse(pos, 1,2), gap=0, cex=.5)
+dev.off()
+
+## If I arbitrarily set L4 to be positive does it affect the covariances?
+xx2 <- xx
+xx2[,4] <- abs(xx2[,4])
+covs <- data.frame(t(apply(xx2, 1, L_to_cov)))
+names(covs) <- paste0('cov_vec', 1:6)
+png('plots/cov_vec_test2.png', width=9, height=7, res=500, units='in')
+pairs(covs, col=ifelse(pos, 1,2), gap=0, cex=.5)
+dev.off()
