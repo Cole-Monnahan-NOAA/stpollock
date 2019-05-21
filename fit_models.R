b3aa87a6f72cd3fbcd9a1a0e68c9f4662f30fcad
colemonnahan <monnahc@uw.edu>
2019-05-21 10:50:48 -0700

    Setup to run new IID test

--- fit_models.R
+++ fit_models.R
@@ -2,15 +2,15 @@
 source("startup.R")
 model <- 'combined'
 
-control <- list(seed=121, beta2temporal=TRUE, beta1temporal=TRUE,
-                n_eps1='IID', n_eps2='IID', n_omega1="IID", n_omega2='IID',
-                combinedoff=FALSE, filteryears=TRUE,
-                kappaoff=0, temporal=0, fixlambda=12)
-savedir <- paste0(getwd(), '/fit_IID_test1')
+control <- list(seed=121, beta2temporal=TRUE, n_x=50,
+                n_eps1="IID", n_eps2="IID", n_omega2="IID", n_omega1="IID",
+                beta1temporal=TRUE, filteryears=TRUE,
+                kappaoff=12, temporal=0, fixlambda=12, make_plots=FALSE)
+savedir <- paste0(getwd(), '/fit_base_ST')
 source("prepare_inputs.R")
 Opt <- Optimize(obj=Obj, lower=TmbList$Lower, loopnum=3, getsd=TRUE,
                 upper=TmbList$Upper,   savedir=savedir,
-                newtonsteps=1, control=list(trace=10))
+                newtonsteps=0, control=list(trace=10))
 results <- process.results(Opt, Obj, Inputs, model, space, savedir)
 plot.vastfit(results)
 
