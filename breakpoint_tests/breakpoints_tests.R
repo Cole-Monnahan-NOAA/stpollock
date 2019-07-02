ccc5e2779ea26aebf3a252ac91168f94e1edb915
colemonnahan <monnahc@uw.edu>
2019-07-02 09:27:14 -0700

    Setup and run breakpoint tests

--- breakpoint_tests/breakpoints_tests.R
+++ breakpoint_tests/breakpoints_tests.R
@@ -17,6 +17,8 @@ ats4 <- cbind(breakpoints='.5_12', read.csv('data/ats_.5_12.csv'))
 ## ats2 <- ats2[,names(ats1)]
 ## ats3 <- ats3[,names(ats1)]
 ## ats4 <- ats4[,names(ats1)]
+## rbind(ats1, ats2, ats3, ats4) %>% group_by(breakpoints) %>%
+##   summarize(total1=sum(strata1), total2=sum(strata2), total3=sum(strata3))
 
 ats <- rbind(ats1, ats2, ats3, ats4) %>%
   select(-X, -surface, -ground, -dist, -strata1) %>%
@@ -122,3 +124,34 @@ Opt <- Optimize(obj=Obj, lower=TmbList$Lower, loopnum=3, getsd=TRUE,
                 newtonsteps=1, control=list(trace=1))
 results <- process.results(Opt, Obj, Inputs, model, space, savedir)
 plot.vastfit(results, plotmaps=TRUE)
+
+
+## Compare the indices from the independent ones
+setwd('breakpoint_tests/')
+load('breakpoints_3_16_ats/Save.RData')
+index3_16_raw <- read.csv('breakpoints_3_16_ats/index.raw.csv')
+index3_16 <- Save$Index
+load('breakpoints_.5_16_ats/Save.RData')
+index.5_16_raw <- read.csv('breakpoints_.5_16_ats/index.raw.csv')
+index.5_16 <- Save$Index
+load('breakpoints_.5_12_ats/Save.RData')
+index.5_12_raw <- read.csv('breakpoints_.5_12_ats/index.raw.csv')
+index.5_12 <- Save$Index
+load('breakpoints_.5_20_ats/Save.RData')
+index.5_20_raw <- read.csv('breakpoints_.5_20_ats/index.raw.csv')
+index.5_20 <- Save$Index
+setwd('..')
+
+index.model <- rbind(cbind(breakpoint='3_16', index3_16),
+                  cbind(breakpoint='.5_16', index.5_16),
+                  cbind(breakpoint='.5_20', index.5_20),
+                  cbind(breakpoint='.5_12', index.5_12))
+index.raw <- rbind(cbind(breakpoint='3_16', index3_16_raw),
+                  cbind(breakpoint='.5_16', index.5_16_raw),
+                  cbind(breakpoint='.5_20', index.5_20_raw),
+                  cbind(breakpoint='.5_12', index.5_12_raw))
+
+g <- ggplot(index.model, aes(year, est, color=breakpoint)) + geom_line()
+ggsave('plots/breakpoints_index_model.png', g, width=7, height=5)
+g <- ggplot(subset(index.raw, type=='Naive'), aes(year, log(value), color=breakpoint)) + geom_line()
+ggsave('plots/breakpoints_index_raw.png', g, width=7, height=5)
