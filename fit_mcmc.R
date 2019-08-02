002f2f5cfce9f6dd7cea0d70338378cb59c5e3fe
colemonnahan <monnahc@uw.edu>
2019-08-02 15:08:06 -0700

    Clean up and run some more fits

--- fit_mcmc.R
+++ fit_mcmc.R
@@ -8,7 +8,7 @@ iter <- 800
 warmup <- 200
 
 
-## Base case for paper: combined w/ constant catchability in P1
+## Base case for paper: combined
 control <- list(model='combined', n_x=100,
                 n_eps1="IID", n_eps2="IID", n_omega2="IID", n_omega1="IID",
                 make_plots=TRUE)
@@ -22,10 +22,10 @@ saveRDS(object = fit, file=paste0(savedir,'/mcmcfit.RDS'))
 plot.mcmc(Obj, savedir, fit)
 
 ## Base case for paper: ATS
-control <- list(model='ats', n_x=200,
+control <- list(model='ats', n_x=100,
                 n_eps1=1, n_eps2=1, n_omega2=1, n_omega1=1,
                 make_plots=TRUE)
-savedir <- paste0(getwd(), '/mcmcfit_200_ats')
+savedir <- paste0(getwd(), '/mcmcfit_100_ats')
 source("prepare_inputs.R")
 fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
                iter=iter, open_progress=FALSE, warmup=warmup,
@@ -35,10 +35,10 @@ saveRDS(object = fit, file=paste0(savedir,'/mcmcfit.RDS'))
 plot.mcmc(Obj, savedir, fit)
 
 ## Base case for paper: BTS
-control <- list(model='bts', n_x=200,
+control <- list(model='bts', n_x=100,
                 n_eps1=1, n_eps2=1, n_omega2=1, n_omega1=1,
                 make_plots=TRUE)
-savedir <- paste0(getwd(), '/mcmcfit_200_bts')
+savedir <- paste0(getwd(), '/mcmcfit_100_bts')
 source("prepare_inputs.R")
 fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
                iter=iter, open_progress=FALSE, warmup=warmup,
@@ -49,30 +49,5 @@ plot.mcmc(Obj, savedir, fit)
 
 
 
-### lambda tests
-## only lambda2
-control <- list(model='combined', n_x=100, fixlambda=1,
-                n_eps1="IID", n_eps2="IID", n_omega2="IID", n_omega1="IID",
-                make_plots=FALSE)
-savedir <- paste0(getwd(), '/mcmcfit_100_combined_lambda2')
-source("prepare_inputs.R")
-fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
-               iter=iter, open_progress=FALSE, warmup=warmup,
-               init=prior.fn, thin=1,
-               control=list(max_treedepth=td, adapt_delta=ad))
-saveRDS(object = fit, file=paste0(savedir,'/mcmcfit.RDS'))
-plot.mcmc(Obj, savedir, fit)
-## only lambda1
-control <- list(model='combined', n_x=100, fixlambda=2,
-                n_eps1="IID", n_eps2="IID", n_omega2="IID", n_omega1="IID",
-                make_plots=FALSE)
-savedir <- paste0(getwd(), '/mcmcfit_100_combined_lambda1')
-source("prepare_inputs.R")
-fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
-               iter=iter, open_progress=FALSE, warmup=warmup,
-               init=prior.fn, thin=1,
-               control=list(max_treedepth=td, adapt_delta=ad))
-saveRDS(object = fit, file=paste0(savedir,'/mcmcfit.RDS'))
-plot.mcmc(Obj, savedir, fit)
 
 
