beaf85ce674107251711bc12fd1fe3f6dd04af83
colemonnahan <monnahc@uw.edu>
2019-08-08 09:11:48 -0700

    Setup catchability sensitivity to run

--- sensitivities/run_catchability.R
+++ sensitivities/run_catchability.R
@@ -4,10 +4,18 @@
 ### time-varying lambda (with tight prior) and (2) both lambda1 and lambda2
 ### constant
 
+chains <- 4
+options(mc.cores = chains)
+source('startup.R')
+td <- 15
+ad <- .9
+iter <- 800
+warmup <- 200
+
 ## Base case for paper: combined w/ time-varying catchability in P1.
 control <- list(model='combined', n_x=100, fixlambda=-1,
                 n_eps1="IID", n_eps2="IID", n_omega2="IID", n_omega1="IID",
-                make_plots=F)
+                make_plots=FALSE)
 savedir <- paste0(getwd(), '/mcmcfit_100_combined_tvlambda1')
 source("prepare_inputs.R")
 fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
@@ -20,8 +28,21 @@ plot.mcmc(Obj, savedir, fit)
 ## Base case for paper: combined w/ kappa1 and kappa2
 control <- list(model='combined', n_x=100, fixlambda=0,
                 n_eps1="IID", n_eps2="IID", n_omega2="IID", n_omega1="IID",
-                make_plots=F)
+                make_plots=FALSE)
 savedir <- paste0(getwd(), '/mcmcfit_100_combined_lambda12')
+source("prepare_inputs.R")
+fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
+               iter=iter, open_progress=FALSE, warmup=warmup,
+               init=prior.fn, thin=1,
+               control=list(max_treedepth=td, adapt_delta=ad))
+saveRDS(object = fit, file=paste0(savedir,'/mcmcfit.RDS'))
+plot.mcmc(Obj, savedir, fit)
+
+## Base case for paper: combined w/ kappa2
+control <- list(model='combined', n_x=100, fixlambda=1,
+                n_eps1="IID", n_eps2="IID", n_omega2="IID", n_omega1="IID",
+                make_plots=FALSE)
+savedir <- paste0(getwd(), '/mcmcfit_100_combined_lambda2')
 source("prepare_inputs.R")
 fit <- tmbstan(Obj, lower=TmbList$Lower, upper=TmbList$Upper, chains=chains,
                iter=iter, open_progress=FALSE, warmup=warmup,
